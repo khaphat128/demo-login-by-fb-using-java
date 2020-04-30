3fbb43a60b68204d3cc6ab9b027147c251aeec02
Your name here <vinhphat@Trans-MacBook-Pro.local>
2020-04-30 21:49:46 +0700

    work hard for food and money

---
+++ src/main/java/com/demo/login/fb/demo/configure/OAu2Config.java
@@ -0,0 +1,22 @@
+package com.demo.login.fb.demo.configure;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+@Configuration
+public class OAu2Config extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/", "/login**", "/webjars/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and().logout().logoutSuccessUrl("/").permitAll()
+                .and().oauth2Login();
+
+    }
+}
